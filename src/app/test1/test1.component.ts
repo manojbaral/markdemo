6d40550f7d0ed7dad5fb4133272b916c797d9f2b
Le Deng <ray.deng83@gmail.com>
2018-05-22 14:11:35 -0400

    add draw multiple

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit, ViewChild, ElementRef, Input} from '@angular/core';
-import { Observable, of, fromEvent, interval, Subscription} from 'rxjs';
+import { Component, OnInit, ViewChild, ViewChildren, ElementRef, QueryList, ChangeDetectionStrategy, ChangeDetectorRef} from '@angular/core';
+import { Observable, of, fromEvent, interval, Subscription, BehaviorSubject} from 'rxjs';
 import { KonvaComponent } from 'ng2-konva';
 
 @Component({
   selector: 'app-test1',
   templateUrl: './test1.component.html',
   styleUrls: ['./test1.component.css']
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Test1Component implements OnInit {
-  @ViewChild('rect1') rect1: KonvaComponent;
+  // @ViewChild('rect1') rect1: KonvaComponent;
   @ViewChild('layer') layer: KonvaComponent;
   @ViewChild('stage') stage: KonvaComponent;
 
+  @ViewChildren('rect') rectList: QueryList<KonvaComponent>;
+
+
+  private index: number=0;
   private mouseX;
   private mouseY;
   private startX;
@@ -20,6 +25,11 @@ export class Test1Component implements OnInit {
   private endY;
   private isDrawing:boolean = false;
   private timer: Subscription;
+  public layerList: Array<any> = [];
+  public rectConfigList: Array<any> = [];
+  public currentRect;
+
+
   public configStage = of({
       width: 800,
       height: 400
@@ -35,27 +45,32 @@ export class Test1Component implements OnInit {
       strokeWidth: 1
     }); 
 
-  constructor() { }
+  constructor(private cd: ChangeDetectorRef) { }
+
+  refresh() {
+    this.cd.detectChanges();
+  }
 
   drawingOn(){
-    let rect = this.rect1.getStage();
+    console.log(this.rectList);
+    this.currentRect = this.rectList.toArray()[this.index].getStage();
+    console.log(this.currentRect);
 
     this.startX = this.mouseX;
     this.startY = this.mouseY;
 
-    rect.x(this.startX);
-    rect.y(this.startY);
-     this.timer = interval(10).subscribe(x => {
+    this.currentRect.x(this.startX);
+    this.currentRect.y(this.startY);
+    this.timer = interval(10).subscribe(x => {
       this.dragging();
       this.layer.getStage().draw();
       // console.log("test");
-      });
+    });
   }
 
   dragging() {
-    let rect = this.rect1.getStage();
-    rect.width(this.mouseX - this.startX);
-    rect.height(this.mouseY- this.startY);
+    this.currentRect.width(this.mouseX - this.startX);
+    this.currentRect.height(this.mouseY- this.startY);
   }
 
   drawingOff(){
@@ -65,22 +80,19 @@ export class Test1Component implements OnInit {
     this.endY = this.mouseY;
     this.redrawShape();
     this.layer.getStage().draw();
+    this.index+=1;
   }
 
   redrawShape(){
-    let rect = this.rect1.getStage();
-    rect.x(this.startX);
-    rect.y(this.startY);
-    rect.width(this.endX - this.startX);
-    rect.height(this.endY - this.startY);
+    this.currentRect.x(this.startX);
+    this.currentRect.y(this.startY);
+    this.currentRect.width(this.endX - this.startX);
+    this.currentRect.height(this.endY - this.startY);
   }
 
   // public handleClick(component) {
   //   console.log('Hello Circle', component);
   // }
-
-   
-
 
   ngOnInit() {
   	const el = document.getElementById('canvas');
@@ -88,15 +100,20 @@ export class Test1Component implements OnInit {
   	const subscription = mouseMoves.subscribe((evt: MouseEvent) => {
   	  this.mouseX = `${evt.clientX}`;
   	  this.mouseY = `${evt.clientY}`;
-
-  	  // When the mouse is over the upper-left of the screen,
-  	  // unsubscribe to stop listening for mouse movements
-  	  // if (evt.clientX < 40 && evt.clientY < 40) {
-  	  //   subscription.unsubscribe();
-  	  // }
   	});
 
-    
-
+    for(let i=0; i<50; i++) {
+      this.rectConfigList.push(
+        new BehaviorSubject({
+          x: 0,
+          y: 0,
+          width: 0,
+          height: 0,
+          fill: 'transparent',
+          stroke: 'black',
+          strokeWidth: 1
+        })
+      );
+    }
 	 }
 }
